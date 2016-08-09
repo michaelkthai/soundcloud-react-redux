98c7354aeec599e8e48e8354f924c7e95e292120
michaelkthai <mthai27@gmail.com>
2016-08-08 23:36:29 -0400

    Made adjustments to test to respect new track data structure.

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
-import * as actions from './actions';
 import App from './components/App';
 import Callback from './components/Callback';
 import Stream from './components/Stream';
 
 const store = configureStore();
-store.dispatch(actions.setTracks(tracks));
 
 const history = syncHistoryWithStore(browserHistory, store);
 
