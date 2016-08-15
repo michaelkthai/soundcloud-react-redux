0e03ede112adb36a0ba3aff7ba99d2bbfe19d983
michaelkthai <mthai27@gmail.com>
2016-08-14 22:21:23 -0400

    Added some basic styles

--- src/components/Stream/presenter.js
+++ src/components/Stream/presenter.js
@@ -19,26 +19,74 @@ class Stream extends Component {
   }
 
   render () {
+
+    var streamStyle = {
+      minHeight: 650,
+      backgroundColor: 'mintcream',
+      fontFamily: 'Helvetica',
+      width: '60%',
+      margin: '4rem auto',
+      padding: '5rem',
+      border: '1px solid sandybrown',
+    };
+    var loginButton = {
+      fontSize: '2rem',
+      color: 'turquoise',
+      backgroundColor: 'white'
+    };
+    var centered = {
+      width: 'auto',
+      textAlign: 'center',
+      margin: 'auto'
+    };
+    var trackListing = {
+      width: '70%',
+      textAlign: 'left',
+      margin: '5% auto',
+    };
+    var playButton = {
+      // width: '8rem',
+      fontSize: '1.5rem',
+      color: 'turquoise',
+      backgroundColor: 'white',
+      // marginTop: '.5rem',
+      marginLeft: '100%'
+
+    };
+    var listingRow = {
+      height: '3rem',
+      textAlign: 'right'
+    };
+
+
     const { user, tracks = [], activeTrack, onAuth, onPlay } = this.props;
 
     return (
-      <div>
-        <div>
+      <div style={streamStyle}>
+        <div style={centered}>
           {
             user ?
-              <div>{user.username}</div> :
-              <button onClick={onAuth} type="button">Login</button>
+              <div><h1>{user.username}'s Stream:</h1></div> :
+              <div><button style={loginButton}onClick={onAuth} type="button">Login</button></div >
+
           }
         </div>
         <br/>
-        <div>
+        <div style={trackListing}>
           {
             tracks.map((track, key) => {
               return (
-                <div className="track" key={key}>
-                  {track.origin.title}
-                  <button type="button" onClick={() => onPlay(track)}>Play</button>
-                </div>
+                <tr style={listingRow}>
+                  <td>
+                    <div className="track" key={key}>
+                    {track.origin.title}</div>
+                  </td>
+                  <td>
+                    <div>
+                      <button style={playButton} type="button" onClick={() => onPlay(track)}>Play</button>
+                    </div>
+                  </td>
+                </tr>
               );
             })
           }
